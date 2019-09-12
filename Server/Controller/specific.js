860a1877a8fb02d8b072c67f18255ad137e3e8d6
Assoumpta <imbassoumpta@gmail.com>
2019-09-12 03:02:58 -0700

    ft(MyDiary):Create an endpoint of getting a specific entry by id.                                                                                         -Created a branch in Develop (ft-specific-168373214)                                                                                                        -Created a file called specific.js that contain condition of get sepcific entry                                                                            -Used npm start command to see if my server is running on PORT 3000                                                                                         -use Postman to check if I can get specific entry by it's id.                                                                                                            [Finished 168373214]

---
+++ Server/Controller/specific.js
@@ -0,0 +1,22 @@
+ import Models from '../Models/db';
+ 
+const specific= function (req, res) {
+    let found = Models.find(function (item) {
+        return item.id === parseInt(req.params.id);
+    });
+    if (found) {
+         res.status(200).json(found)
+    
+    
+    } else {
+        return res.status(404).send({
+            status:404,
+            message:'Index not found',
+      
+    })
+    }
+}
+module.exports=specific;
+
+
+ 
--- Server/Routes/entryRoutes.js
+++ Server/Routes/entryRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import getAll from '../Controller/getall';
-
+import specific from '../Controller/specific';
 const router = express.Router();
 
 
 router.get('/api/v1/entries', getAll);  
-
+router.get('/api/v1/entries/:id', specific);
 module.exports=router;
