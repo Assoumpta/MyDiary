bcc1eb89892a9354803ccd5f4ed768b0c3bf2d7a
Assoumpta <imbassoumpta@gmail.com>
2019-09-13 06:13:09 -0700

    ft(MyDiary):Create an endpoint of creating new entry and be added in array.                                                                                         -Created a branch in Develop (ft-add-168374160)                                                                                                        -Created a file called addentry.js that contain condition of add new entry                                                                            -Used npm start command to see if my server is running on PORT 3000                                                                                         -use Postman to check if I can add new entry using POST                                                                                                            [Finished 168374160]

--- Server/Routes/entryRoutes.js
+++ Server/Routes/entryRoutes.js
@@ -1,15 +1,15 @@
 import express from 'express';
 //import bodyParser from 'body-parser';
-import getAll from '../Controller/getall';
-import specific from '../Controller/specific';
-import add from '../Controller/addentry';
+import getAllentriesl from '../Controller/getallentriesl';
+import getspecificentry from '../Controller/getspecificentry';
+import addentry from '../Controller/addentry';
 import bodyParser from 'body-parser';
 const router = express.Router();
 
 
-router.get('/api/v1/entries', getAll);  
-router.get('/api/v1/entries/:id', specific);
-router.post('/api/v1/entries/' ,add);
+router.get('/api/v1/entries', getAllentriesl);  
+router.get('/api/v1/entries/:id', getspecificentry);
+router.post('/api/v1/entries/' ,addentry);
 
 
 module.exports=router;
