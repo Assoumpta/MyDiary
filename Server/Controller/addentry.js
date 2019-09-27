2d8a97934aa8813b63c0aa735b0fb2e1d2cd8d93
Assoumpta <imbassoumpta@gmail.com>
2019-09-27 09:40:59 -0700

    bg(MyDiary):Created test for all endpoints                                                                                         -Created a branch in Develop (bg-test-api-168722015)                                                                                                        -Created a folder called test which contains tes.js where to write the test                                                                            -Used npm run tes command to test my endpoints if they are passing                                                                                         -use Eslint to improve the quality of my codes                                                                                                            [Finished 168722015]

--- Server/Controller/addentry.js
+++ Server/Controller/addentry.js
@@ -1,34 +1,28 @@
 import Models from '../Models/db';
 
-
-  const addentry = function(req, res)  {
-    if(!req.body.title) {
+const addentry = (req, res) => {
+      if (!req.body.title) {
+         return res.status(400).send({
+          success: 'false',
+          description: 'title is required',
+        });
+ } 
+else if (!req.body.description) {
       return res.status(400).send({
         success: 'false',
-        descrition: 'title is required'
-      });
-    } else if(!req.body.description) {
-      return res.status(400).send({
-        success: 'false',
-        description: 'message is required'
-      });
-    }
-    
-    
+        description: 'message is required',
+});
+}
    const addentry = {
      id: Models.length + 1,
      title: req.body.title,
-     description: req.body.description
-   }
+     description: req.body.description,
+   };
    Models.push(addentry);
    return res.status(201).send({
      success: 'true',
-     message: ' entry added successfully',
-     addentry
-   })
+     message: 'entry added successfully',
+     addentry,
+     });
   };
-
-  module.exports=addentry;
-
-
-    
+module.exports = addentry;
