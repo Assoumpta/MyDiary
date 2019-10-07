13e0aba1edd4fec4e5b2531dbe778c504a00b12a
Assoumpta <imbassoumpta@gmail.com>
2019-10-07 05:45:18 -0700

    bg(test-api-168722015)                                                                               -Improve validation                                                                                                -Add regular expression                                                                                              (finishes 168722015)

--- Server/validation/validation.js
+++ Server/validation/validation.js
@@ -1,12 +1,19 @@
 import joi from 'joi';
 
 const entryvalidation = {
+    validateString(req, res, next) {
+        const testString = /^[a-zA-z]+$/.test(req.body.title.toString());
+        console.log(testString)
+        if (!testString) return res.status(400).send('error');
+        return next();
+    },
     validate(schema) {
         const newentry= {
           
-            title: joi.string().trim().required(),
+        title: joi.string().trim().required(),
         description: joi.string().trim().required(),
         };
+
         return joi.validate(schema, newentry);
     },
    };
